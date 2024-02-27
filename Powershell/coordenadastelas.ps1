35ecfcc34a92b66275b7f930c3eee73d662b847b
Kelvyn <52673971+preetebr@users.noreply.github.com>
2024-02-26 21:47:01 -0300

    Delete Powershell directory

--- Powershell/coordenadastelas.ps1
+++
@@ -1,8 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-$screenCount = [System.Windows.Forms.Screen]::AllScreens.Count
-Write-Host "Número de telas conectadas: $screenCount"
-
-for ($i = 0; $i -lt $screenCount; $i++) {
-    $screen = [System.Windows.Forms.Screen]::AllScreens[$i]
-    Write-Host "Tela $i - Resolução: $($screen.Bounds.Width)x$($screen.Bounds.Height), Coordenadas: $($screen.Bounds.X), $($screen.Bounds.Y)"
-}
