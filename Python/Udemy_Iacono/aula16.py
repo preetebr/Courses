556022d9f25a017a5e0b5cb379cd5755b69a780c
kelvyn <kelvynsprete@gmail.com>
2024-06-26 17:00:56 -0300

    Course

---
+++ Python/Udemy_Iacono/aula16.py
@@ -0,0 +1,7 @@
+nome = input("Qual o seu nome: ")
+idade = input("Qual sua idade: ")
+idade = str(idade)
+cidade = input("Onde mora: ")
+
+print('O ' + nome + ' tem ' + idade + ' anos de idade e mora em ' + cidade + '.')
+
---
+++ Python/Udemy_Iacono/aula17.py
@@ -0,0 +1,5 @@
+nascimento = int(input("Qual ano nasceu: ")) 
+idade =  2024 - nascimento
+
+
+print(idade)
