f2f5c651cd23bbc26b4f6b3699157c16d81cb336
kelvyn <kelvynsprete@gmail.com>
2024-08-14 18:24:02 -0300

    Course

---
+++ Python/Geek_university/guppe/pep8.py
@@ -0,0 +1,30 @@
+"""
+PEP 8 Python
+
+São propostas de melhorias para a linguagem Python
+
+The Zen of Python
+
+import this
+
+
+A ideia do PEP 8 é que possamos escrever códigos Python de forma Pythônica.
+
+[1] -  Utilize Camel Case para nomes de classes;
+[2] - Utiliza nomes em minúsculos, separados por underline para funções ou variavéis;
+[3] - Utilize 4 espaços para identação;
+[4] - Linhas em branco;
+- Separar funções e definições de classe com duas linhas em branco;
+- Métodos dentro de uma classe devem ser separados com uma única linha em branco;
+
+[5] - Imports
+- Imports devem ser sempre feitos em linhas separadas;
+
+[6] - Espaços em expressões e instruções
+
+
+"""
+
+
+
+
